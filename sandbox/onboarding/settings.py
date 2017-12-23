a563df37a750d3ff9daa7547604beb2359e87abd
Weronika Felczak <weronika.dominiak@hotmail.com>
2017-12-24 00:01:32 +0100

    Initial commit

--- sandbox/onboarding/settings.py
+++ sandbox/onboarding/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')7+-x_br7km-(0j3&n)%vx@(^^0ifbu-t@4f1)((j23#-cmvk)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'weronikaf.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
